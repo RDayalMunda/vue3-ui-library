d573d32e57fce080d8f1170da87dab81090cb232
Ram <ramdayal.m@gsl.in>
2025-08-08 21:19:45 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hellow World</h1>
+    <RdmInput v-model="name" label="Name" placeholder="Enter your name" />
+  </div>
+</template>
+
+<script setup>
+import RdmInput from './components/RdmInput.vue'
+import { ref } from 'vue'
+
+const name = ref('')
+</script>
---
+++ src/components/RdmInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="rdm-input-container">
+    <label class="rdm-input-label">{{ label }}</label>
+    <input
+      class="rdm-input"
+      :type="type"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      :value="modelValue"
+      @input="updateValue"
+    />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  label: {
+    type: String,
+    default: '',
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  modelValue: {
+    type: String,
+    default: '',
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+})
+
+function updateValue(event) {
+  emit('update:modelValue', event.target.value)
+}
+</script>
