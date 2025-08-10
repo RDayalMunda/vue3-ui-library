4ded6301ac73be8e7ca2e8c2281e6ff48a3bb0c8
Ram <ramdayal.m@gsl.in>
2025-08-10 16:25:08 +0530

    Input field style fix

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,50 @@
 <template>
-  <div>
+  <div class="main-container">
     <h1>Hellow World</h1>
-    <div style="display: flex; flex-direction: column; gap: 10px; padding: 50px">
+    <pre class="models">
+name = {{ name }}
+password = {{ password }}
+    </pre>
+    <div class="container">
       <RdmInput v-model="name" label="Name" placeholder="Enter your name" />
+      <RdmInput
+        v-model="password"
+        label="Password"
+        placeholder="Enter your password"
+        type="password"
+      />
       <RdmButton text="Click me" :clickHandler="handleClick" />
     </div>
   </div>
 </template>
 
 <script setup>
-import RdmInput from './components/RdmInput.vue'
+import RdmInput from './stories/RdmInput/RdmInput.vue'
 import RdmButton from './stories/RdmButton/RdmButton.vue'
 import { ref } from 'vue'
 
 const name = ref('')
+const password = ref('')
 function handleClick() {
-  alert('clicked: ' + name.value)
+  console.log('clicked: ', name.value, password.value)
 }
 </script>
+<style scoped>
+.main-container {
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  background-color: #4a4a4a;
+}
+.models {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+</style>
